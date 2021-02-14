ea2554224db4b515139185e5c0233831bc13e2d5
phoenix-programmer <diego.faz@gmail.com>
2021-02-14 10:52:26 -0300

    Changed @DisplayName to @ParameterizedTest(name = ...) in parametrized tests

--- fractions/test/source/org/shevek/fractions/FractionTest.java
+++ fractions/test/source/org/shevek/fractions/FractionTest.java
@@ -8,17 +8,15 @@ import static org.assertj.core.api.Assertions.*;
 
 class FractionTest {
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "should have denominator equals to one for integers - {1}")
     @CsvSource({"0", "1", "2", "-3"})
-    @DisplayName("should have denominator equals to One for integers")
     void integersWhenRepresentedAsFractionShouldHaveDenominatorEqualToOne(int integer) {
         final Fraction integerAsFraction = Fraction.forInteger(integer);
         assertThat(integerAsFraction.toString()).isEqualTo(String.format("%d/1", integer));
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "should return same result for integer fraction addition than when adding two integers")
     @CsvSource({"2, 3, 5", "4, 3, 7", "3, 0, 3", "0, 0, 0"})
-    @DisplayName("should return same result for integer fraction addition than when adding two integers")
     void integerFractionRepresentationAdditionShouldYieldSameResultAsIntegerAddition(int a, int b, int c) {
         final Fraction aAsFraction = Fraction.forInteger(a);
         final Fraction bAsFraction = Fraction.forInteger(b);
@@ -26,31 +24,28 @@ class FractionTest {
         assertThat(aAsFraction.add(bAsFraction).toString()).isEqualTo(String.format("%s/1", c));
     }
 
-    @ParameterizedTest
-    @CsvSource({"1/2", "2/3", "5/3"})
-    @DisplayName("string factory method should produce a printable fracton equals to its input")
-    void stringFactoryMethodTest(String fractionAsString) {
+    @ParameterizedTest(name = "should use improper notation when representing fraction as string: {0}")
+    @CsvSource({"7/5", "11/4", "5/3"})
+    void shouldUseImproperNotation(String fractionAsString) {
         Fraction fraction = Fraction.forString(fractionAsString);
 
         assertThat(fraction.toString()).isEqualTo(fractionAsString);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "string factory method should deal with integers")
     @CsvSource({"1, 1/1", "2, 2/1"})
-    @DisplayName("string factory method should deal with integers")
     void stringFactoryMethodShouldDealWithIntegers(String integerAsString, String expectedResult) {
         assertThat(Fraction.forString(integerAsString).toString()).isEqualTo(expectedResult);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "string factory method should not accept invalid input")
     @CsvSource({"A", "1/2/3", "1/A"})
-    @DisplayName("string factory method should not accept invalid input")
     void stringFactoryMethodShouldNotAcceptInvalidInput(String input) {
         assertThatIllegalArgumentException().isThrownBy(() -> Fraction.forString(input));
     }
 
     @Test
-    @DisplayName("can't have 0 as denominator")
+    @DisplayName("Fractions can't have 0 as denominator")
     void cantHaveZeroAsDenominator() {
         assertThatIllegalArgumentException().isThrownBy(() -> new Fraction(5, 0)).withMessage("0 isn't a valid denominator");
         assertThatIllegalArgumentException().isThrownBy(() -> Fraction.forString("3/0")).withMessage("0 isn't a valid denominator");
@@ -71,16 +66,10 @@ class FractionTest {
     }
 
     @Test
-    @DisplayName("shouldUseLowestTerms")
+    @DisplayName("should use lowest terms when representing a Fraction as string")
     void shouldUseLowestTermsWhenExpressingAsString() {
         Fraction fraction = Fraction.forString("2/4");
         assertThat(fraction.toString()).isEqualTo("1/2");
     }
 
-    @Test
-    @DisplayName("shouldUseLowestTermsWhenAdding")
-    void shouldUseLowestTermsWhenAdding() {
-        Fraction fraction = Fraction.forString("1/3");
-        assertThat(fraction.add(Fraction.forString("8/3")).toString()).isEqualTo("3/1");
-    }
 }
